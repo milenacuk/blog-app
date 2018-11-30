53aa11446016b072b4708f9466e957c675e5992d
Milena <milenacuk89@gmail.com>
2018-11-30 19:32:27 +0100

    Zadatak 2

--- src/components/AppPost.vue
+++ src/components/AppPost.vue
@@ -1,10 +1,30 @@
 <template>
-    
+<div>
+    <div v-for="(post,index) in posts" :key="index">
+            <h2 class="blog-post-title"><router-link to="/posts"> {{post.title }}</router-link></h2>
+            <!-- <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p> -->
+
+            <p>{{ post.text }}</p>
+          </div>
+</div>
 </template>
 
 
 <script>
+import { posts } from '../services/Posts.js'
+
 export default {
-    
+    data(){
+        return{
+            posts:[]
+        }
+    },
+    created(){
+        posts.getAll().then(response => {
+            this.posts = response.data;
+        }).catch(error => {
+            console.log(error.response);
+        })
+    }
 }
 </script>
